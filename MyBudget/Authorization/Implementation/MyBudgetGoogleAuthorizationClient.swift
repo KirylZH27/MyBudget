530faa38eb2c006ef241aad813cf18b972645097
KirylZhahalski <Kirill270202@icloud.com>
2023-07-01 13:11:30 +0300

    Added GoogleSignIn Implementation

---
+++ MyBudget/Authorization/Implementation/MyBudgetGoogleAuthorizationClient.swift
@@ -0,0 +1,40 @@
+//
+//  MyBudgetGoogleAuthorizationClient.swift
+//  MyBudget
+//
+//  Created by Кирилл Жогальский on 1.07.23.
+//
+
+import GoogleSignIn
+
+final class MyBudgetGoogleAuthorizationClient: GoogleAuthorizationClient {
+    
+    enum Error: Swift.Error {
+        case invalidGoogleAuthResult
+    }
+    
+    private let clientId: String
+    private let presentationController: UIViewController
+    
+    init(clientId: String, presentationController: UIViewController) {
+        self.clientId = clientId
+        self.presentationController = presentationController
+    }
+    
+    func requestAuthorization(completion: @escaping (GoogleAuthorizationClient.Result) -> Void) {
+        let config = GIDConfiguration(clientID: clientId)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentationController) { result, error in
+            if let error = error{
+                return completion(.failure(error))
+            }
+            guard let user = result?.user,
+                    let idToken = user.idToken?.tokenString else {
+                return completion(.failure(Error.invalidGoogleAuthResult))}
+            completion(.success(GoogleAuthorizationResult(idToken: idToken, accessToken: user.accessToken.tokenString)))
+        }
+    }
+    
+    
+}
