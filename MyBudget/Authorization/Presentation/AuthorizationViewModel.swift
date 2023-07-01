6c1fd35fb5c6969b47a2f7cbb0f801eaa23e5f4a
KirylZhahalski <Kirill270202@icloud.com>
2023-07-01 13:42:25 +0300

    Added realization Firebase Authorization

--- MyBudget/Authorization/Presentation/AuthorizationViewModel.swift
+++ MyBudget/Authorization/Presentation/AuthorizationViewModel.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 final class AuthorizationViewModel {
+    private let authorizationManager: AuthorizationManager
     
+    init(authorizationManager: AuthorizationManager) {
+        self.authorizationManager = authorizationManager
+    }
+    func requestGoogleAuthorization() {
+        authorizationManager.requestAuthorization(in: .google) { [weak self] result in
+            <#code#>
+        }
+    }
 }
---
+++ MyBudget/Firebase/Authorization/FirebaseAuthorizationManager.swift
@@ -0,0 +1,71 @@
+//
+//  FirebaseAuthorizationManager.swift
+//  MyBudget
+//
+//  Created by Кирилл Жогальский on 1.07.23.
+//
+
+import FirebaseAuth
+import FirebaseFirestore
+
+protocol FirebaseAuth {
+    func requestUserId(with credential: AuthCredential, completion: @escaping(String?, Error?) -> Void)
+}
+
+extension Auth: FirebaseAuth {
+    func requestUserId(with credential: AuthCredential, completion: @escaping (String?, Error?) -> Void) {
+        signIn(with: credential) { authResult, error in
+            completion(authResult?.user.uid, error)
+        }
+    }
+}
+
+final class FirebaseAuthorizationManager: AuthorizationManager {
+    
+    private let googleAuthorizationClient: GoogleAuthorizationClient
+    private var completion: ((AuthorizationResult) -> Void)?
+    private let auth: FirebaseAuth
+    
+    enum Error: Swift.Error {
+        case missingUserId
+    }
+    
+    init(googleAuthorizationClient: GoogleAuthorizationClient, auth: FirebaseAuth) {
+        self.googleAuthorizationClient = googleAuthorizationClient
+        self.auth = auth
+    }
+    
+    
+    func requestAuthorization(in type: AuthorizationType, completion: @escaping (AuthorizationResult) -> Void) {
+        switch type {
+            case .google:
+                googleAuthorizationClient.requestAuthorization { [weak self] result in
+                    switch result {
+                        case .success(let authResult):
+                            self?.signInFirebase(with: authResult)
+                        case .failure(let error):
+                            completion(.failure(error))
+                    }
+                }
+        }
+        self.completion = completion
+    }
+    
+    private func signInFirebase(with authorizationResult: GoogleAuthorizationResult) {
+        let credential = GoogleAuthProvider.credential(withIDToken: authorizationResult.idToken, accessToken: authorizationResult.accessToken)
+        signInFirebase(with: credential)
+    }
+    
+    private func signInFirebase(with credentials: AuthCredential) {
+        auth.requestUserId(with: credentials) { [weak self] (userId, error) in
+            if let error = error {
+                self?.completion?(.failure(error))
+                return
+            }
+            guard let userId = userId else {
+                self?.completion?(.failure(Error.missingUserId))
+                return }
+            self?.completion?(.success(userId))
+        }
+    }
+}
