90b4008157a10ce9dc667d65e030913b625bb507
KirylZhahalski <Kirill270202@icloud.com>
2023-07-08 18:11:37 +0300

    Added helpers: ImagePicker, BlockTap in project

---
+++ MyBudget/Helpers/BlockTap.swift
@@ -0,0 +1,49 @@
+//
+//  BlockTap.swift
+//  MyBudget
+//
+//  Created by Кирилл Жогальский on 8.07.23.
+//
+
+import UIKit
+
+class BlockTap: UITapGestureRecognizer {
+    fileprivate var tapAction: ((UITapGestureRecognizer) -> Void)?
+    
+    public override init(target: Any?, action: Selector?) {
+        super.init(target: target, action: action)
+    }
+    
+    public convenience init (tapCount: Int = 1,
+                             fingerCount: Int = 1,
+                             action: ((UITapGestureRecognizer) -> Void)?) {
+        
+        self.init()
+        self.numberOfTapsRequired = tapCount
+        
+        #if os(iOS)
+        self.numberOfTouchesRequired = fingerCount
+        #endif
+        
+        self.tapAction = action
+        self.addTarget(self, action: #selector(BlockTap.didTap(_:)))
+    }
+    
+    @objc open func didTap (_ tap: UITapGestureRecognizer) {
+        tapAction? (tap)
+    }
+}
+
+
+extension UIView {
+    
+    public func addTapGesture(tapNumber: Int = 1,
+                              action: ((UITapGestureRecognizer) -> ())?) {
+        
+        let tap = BlockTap(tapCount: tapNumber,
+                           fingerCount: 1,
+                           action: action)
+        
+        addGestureRecognizer(tap)
+    }
+}
--- MyBudget/SceneDelegate.swift
+++ MyBudget/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let rootViewController = MyBudgetDependencyContainer(sharedWindow: window!).makeMainViewController()
-        try? Auth.auth().signOut()
+        //try? Auth.auth().signOut()
         window?.rootViewController = rootViewController
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
